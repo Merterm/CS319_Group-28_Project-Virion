e24d71fc9261f901a6ea1eeeaa1a520533d30471
whitehatgooner <bekathegooner@gmail.com>
2016-12-10 01:49:39 +0300

    methods of uicontroller

--- src/gamemanagement/UIController.java
+++ src/gamemanagement/UIController.java
@@ -1,5 +1,8 @@
 package gamemanagement;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*
 /**
  * CS319: Object-Oriented Software Engineering Course Project
  * Project Virion
@@ -7,5 +10,44 @@ package gamemanagement;
  * Version: 1.0
  * Description:
  */
-public class UIController {
+public class UIController
+{
+    public void openInfoPane()
+    {
+        JFrame infoFrame = new JFrame("Game Info");
+        infoFrame.setLayout(new FlowLayout());
+        infoFrame.setVisible(true);
+        infoFrame.setSize(400,400);
+        infoFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new FlowLayout());
+        infoPanel.setVisible(true);
+        JLabel infoLabel = new JLabel("Information");
+
+        infoPanel.add("label",infoLabel);
+        infoFrame.add("panel",infoPanel);
+    }
+
+    public void openHelpPane()
+    {
+        //...
+    }
+
+    public void openDNAFinderWindow(int[] dnaLocations)
+    {
+        JWindow postInfecWindow = new JWindow();
+        //...
+    }
+
+    public void pause()
+    {
+        //...
+    }
+
+    public void changePosition()
+    {
+        //...
+    }
+
 }
