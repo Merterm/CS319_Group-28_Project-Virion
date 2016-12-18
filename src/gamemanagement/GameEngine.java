7df87e05d5cfc41628eec38736ae9f70bb30d6e2
Irmak <irmak.yilmaz.ug@gmail.com>
2016-12-18 18:34:02 +0300

    added 3 methods to GameEngine classes. Make some little changes on Protein Virus and Cell

--- src/gamemanagement/GameEngine.java
+++ src/gamemanagement/GameEngine.java
@@ -4,6 +4,7 @@ import gamecomponents.protein.Protein;
 import gamecomponents.virus.ViralDNA;
 import gamecomponents.virus.Virus;
 
+import java.util.Timer;
 import java.util.Vector;
 
 /**
@@ -18,6 +19,10 @@ public class GameEngine
     /*NOTE: there are separate lists(vector) for viruses,proteins,etc.
      * ID of an object can be index of the list */
     //todo hash hashmap hashtable map dict
+    private int score;
+    private int time;
+    private int cmCount;
+    private int atpCount;
 
     protected int viralDNACapacity = 100;   //for now 100
     //number of objects are not know at the start -> vectors are dynamic
@@ -38,5 +43,38 @@ public class GameEngine
     {
         proteinList = new Vector<Protein>(2,2);
     }
+
+    //methods
+    public void increaseATP(){
+        for(int i = 0; i>=0; i++) {
+            try {
+                //sending the actual Thread of execution to sleep 3600 milliseconds
+                Thread.sleep(3600);
+            } catch(InterruptedException ie) {}
+            this.atpCount=this.atpCount + 1000;
+        }
+    }
+    public void increaseCM(){
+        for(int i = 0; i>=0; i++) {
+            try {
+                //sending the actual Thread of execution to sleep 3600 milliseconds
+                Thread.sleep(3600);
+            } catch(InterruptedException ie) {}
+            this.cmCount=this.cmCount + 1000;
+        }
+    }
+    public int calculateDenaturationTime(int virusID,int proteinID){
+        int result = 0;
+        for(int i = 0; i<proteinList.size();i++){
+            if(proteinList.get(i).getProteinTypeCoefficient()==proteinID)
+                result= 90* proteinList.get(i).getProteinTypeCoefficient();
+        }
+        for(int i = 0; i<virusList.size();i++){
+            if(virusList.get(i).getVirusTypeCoefficient()==virusID)
+                result=result-virusList.get(i).getVirusTypeCoefficient();
+        }
+        return result;
+    }
+
 
 }
